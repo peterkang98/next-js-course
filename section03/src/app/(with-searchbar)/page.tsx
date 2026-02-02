08ed063f2be586c71c2af8046ca8ce76a5567c4d
강동현 <peterkang98@gmail.com>
2026-02-02 15:25:11 +0900

    스켈레톤 UI 작성 + delay 유틸리티 함수로 확인

--- section03/src/app/(with-searchbar)/page.tsx
+++ section03/src/app/(with-searchbar)/page.tsx
@@ -1,8 +1,12 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import {BookData} from "@/types";
+import {delay} from "@/util/delay";
+import {Suspense} from "react";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 
 async function AllBooks() {
+  await delay(1500);
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, {cache: "force-cache"});
 
   if (!response.ok) return <div>오류가 발생했습니다...</div>;
@@ -16,7 +20,9 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`, { next: {revalidate: 3}});
+  await delay(3000);
+  // const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`, { next: {revalidate: 3}});
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`);
 
   if (!response.ok) return <div>오류가 발생했습니다...</div>;
 
@@ -36,11 +42,15 @@ export default function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks/>
+        <Suspense fallback={<BookListSkeleton count={3}/>}>
+          <RecoBooks/>
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks/>
+        <Suspense fallback={<BookListSkeleton count={3}/>}>
+          <AllBooks/>
+        </Suspense>
       </section>
     </div>
   );
--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,13 +1,10 @@
 import BookItem from "@/components/book-item";
 import {BookData} from "@/types";
+import {delay} from "@/util/delay";
+import {Suspense} from "react";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ q?: string }>;
-}) {
-  const { q } = await searchParams;
-
+async function SearchResult({q}: {q: string}) {
+  await delay(1500);
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`, {cache: "force-cache"});
   console.log(response)
   if (!response.ok) return <div>오류가 발생했습니다...</div>;
@@ -20,5 +17,20 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const { q } = await searchParams;
+
+  return (
+    <Suspense key={q || ""} fallback={<div>Loading ...</div>}>
+      <SearchResult q={q || ""}/>
+    </Suspense>
   );
 }
