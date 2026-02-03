aa44947c519128f20550b21a59ca7c7a545e6144
강동현 <peterkang98@gmail.com>
2026-02-03 16:13:26 +0900

    리뷰 기능 추가

--- section03/src/app/book/[id]/page.tsx
+++ section03/src/app/book/[id]/page.tsx
@@ -1,5 +1,8 @@
 import style from "./page.module.css";
 import {notFound} from "next/navigation";
+import {ReviewData} from "@/types";
+import ReviewItem from "@/components/review-item";
+import ReviewEditor from "@/components/review-editor";
 
 export function generateStaticParams() {
   const arr = [];
@@ -9,12 +12,7 @@ export function generateStaticParams() {
   return arr;
 }
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string | string[] }>;
-}) {
-  const { id } = await params;
+async function BookDetail({id}: { id: string; }) {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`);
 
   if (!response.ok) {
@@ -24,15 +22,15 @@ export default async function Page({
     return <div>오류가 발생했습니다...</div>;
   }
 
-  const { title, subTitle, description, author, publisher, coverImgUrl } = await response.json();
+  const {title, subTitle, description, author, publisher, coverImgUrl} = await response.json();
 
   return (
-    <div className={style.container}>
+    <section>
       <div
         className={style.cover_img_container}
-        style={{ backgroundImage: `url('${coverImgUrl}')` }}
+        style={{backgroundImage: `url('${coverImgUrl}')`}}
       >
-        <img src={coverImgUrl} />
+        <img src={coverImgUrl}/>
       </div>
       <div className={style.title}>{title}</div>
       <div className={style.subTitle}>{subTitle}</div>
@@ -40,6 +38,35 @@ export default async function Page({
         {author} | {publisher}
       </div>
       <div className={style.description}>{description}</div>
+    </section>
+  );
+}
+
+async function ReviewList({bookId}: { bookId: string; }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`,
+    { next: { tags: [`review-${bookId}`] } }
+  );
+  if (!response.ok) {
+    throw new Error(`Review fetch failed: ${response.statusText}`);
+  }
+
+  const reviews: ReviewData[] = await response.json();
+
+  return (
+    <section>
+      {reviews.map(review => <ReviewItem key={`review-item-${review.id}`} {...review}/>)}
+    </section>
+  );
+}
+
+export default async function Page({ params }: { params: Promise<{ id: string }>; }) {
+  const {id} = await params;
+  return (
+    <div className={style.container}>
+      <BookDetail id={id}/>
+      <ReviewEditor bookId={id}/>
+      <ReviewList bookId={id}/>
     </div>
   );
 }
